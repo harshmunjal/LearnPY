292a357778ac209a4b6839662835aa1a99897fcd
Harsh Munjal <harshmunjal@hey.com>
2025-07-25 17:42:38 +0530

    First Commit

---
+++ PYCal.py
@@ -0,0 +1,15 @@
+# Display main menu 
+
+
+# Capture the user's choice
+
+# Activate the Function related to the user's choice
+
+# Display the result
+
+# Ask if the user wants another calculation
+
+# If yes, go back to step 1
+
+# If no, exit the program
+
